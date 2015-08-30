d4b08ce2e7c8643f55e96e17327bc395851b5db0
KINOSHITA Shinji <shinji.kino@gmail.com>
2015-08-30 18:01:19 +0900

    Enable bluetooth

--- burn.sh
+++ burn.sh
@@ -2,5 +2,8 @@
 
 dd if=live-image-amd64.hybrid.iso of=/dev/sdb bs=2048k
 cfdisk /dev/sdb
-umount /dev/sdb2
-mkfs.ext3 /dev/sdb2 -L persistence
+mkfs.ext4 /dev/sdb2 -L persistence
+mount /dev/sdb2 /mnt
+echo "/home" > /mnt/persistence.conf
+umount /mnt
+
