07617aba0b005a4cc642bec77afb15641ba4e807
KINOSHITA Shinji <shinji.kino@gmail.com>
2015-08-31 15:36:34 +0900

    Add some security tools
    * Set zsh as default shell
    * Get persistance encryption working

--- burn.sh
+++ burn.sh
@@ -4,6 +4,6 @@ dd if=live-image-amd64.hybrid.iso of=/dev/sdb bs=2048k
 cfdisk /dev/sdb
 mkfs.ext4 /dev/sdb2 -L persistence
 mount /dev/sdb2 /mnt
-echo "/home" > /mnt/persistence.conf
+echo "/ union" > /mnt/persistence.conf
 umount /mnt
 
